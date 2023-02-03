1b32504c6fd250d7fa40e8b29295e8e4691d3774
lee <lee.li@publicissapient.com>
2023-02-03 17:33:16 +0800

    commit.

--- src/test/java/TT.java
+++ src/test/java/TT.java
@@ -6,7 +6,7 @@ public class TT {
 
     @Test
     public void aaaa() {
-        DataUtil.init("7k/8/8/8/8/8/8/7K b - - 4 80");
-        System.out.println(SearchUtil.get_score(DataUtil.board));;
+        DataUtil.init("7k/8/8/8/8/8/rr6/7K b - - 4 80");
+        System.out.println(SearchUtil.get_score(DataUtil.board));
     }
 }
